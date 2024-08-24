e5786c21aa0ff086449de816b47e5ddf5c4ffb8c
Geraud KAMENI <geraudkameni@gmail.com>
2024-08-24 04:03:25 +0200

    Respond with body

--- app/server.go
+++ app/server.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strings"
 )
+
+func do(conn net.Conn) {
+
+	buff := make([]byte, 1024)
+	n, err := conn.Read(buff)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println(string(buff))
+	request := string(buff[:n])
+	path := strings.Split(request, " ")[1]
+
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(path, "/")[1] == "echo" {
+		message := strings.Split(path, "/")[2]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	conn.Close()
+
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,14 +46,6 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	req := make([]byte, 1024)
-	conn.Read(req)
+	do(conn)
 
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
-		return
-	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
